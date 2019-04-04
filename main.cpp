e1e4fe74c491a26680fc7ea541fc69a2878f6b4b
andresugaz <48999316+andresugaz@users.noreply.github.com>
2019-04-03 23:48:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <numeric>
-//#include <cstdio>
 
 using namespace std;
 int arraySum(int a[9]);
@@ -11,7 +10,7 @@ int arraySum(int a[9]);
 int main() 
 { 	
 const char* lista2[6]={"BCG","BGC","CBG","CGB","GBC","GCB"};  
-    //const char* lista2[6]={"BCG","CGB","GBC","CBG","BGC","GCB"};  
+ 
 int minimos[6]={0,0,0,0,0,0};
 int lista[9];
 
@@ -25,7 +24,6 @@ int lista[9];
     
     int suma = (arraySum(lista));
 	
-	//cout<<suma<<endl;
 
     		minimos[0]=suma-lista[0]-lista[5]-lista[7];
     		minimos[3]=suma-lista[2]-lista[4]-lista[6];
@@ -34,11 +32,6 @@ int lista[9];
 			minimos[1]=suma-lista[0]-lista[4]-lista[8];
 			minimos[5]=suma-lista[1]-lista[5]-lista[6];
 
-	/*for (int z=0;z<9;z++)
-	{
-		cout<<lista[z]<<" ";
-	}
-	cout<<endl;*/
 	int menorNRO = 1000000000;
 	for (int z=0;z<6;z++)
 	{
@@ -46,7 +39,7 @@ int lista[9];
 		{
 			menorNRO=minimos[z];
 		}
-		//cout<<minimos[z]<<" ";
+		
 	}
 	int cont = 0;
 	
@@ -63,20 +56,10 @@ int lista[9];
 		{
 		menorINDEX = z;
 		interruptor = 1;
-		//cont=cont+1;
-		//cont=cont+1;
-		//interruptor = interruptor+1;
+		
 		}
 	}
-	/*if (cont == 1)
-	{
-	    cout<<lista2[menorINDEX]<<" "<<menorNRO<<endl;   	
-	}
-	else
-	    cout<<lista2[menorINDEX]<<" "<<menorNRO<<endl;
-	//cout<<menorNRO<<endl;
 	
-    }*/
     cout<<lista2[menorINDEX]<<" "<<menorNRO<<endl;
 }
     return 0;
@@ -93,21 +76,3 @@ int arraySum(int a[9])
     return initial_sum; 
 } 
   
-/*void ingreso(int V[9]) 
-{
-	for (int i=0;i<10;i++)
-	{
-		int V[9] = {0,0,0,0,0,0,0,0,0};
-		cin>>V[i];
-	//int a,b,c,d,e,f,g,h,i;
-		//while((scanf("%d %d %d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g,&h,&i))==9)
-		//int lista[]={a,b,c,d,e,f,g,h,i};
-		//a=lista[1]
-		//cout<<a;
-	}
-}
-int main()
-{
-	cout<<"Hola";
-    return 0;
-}*/
